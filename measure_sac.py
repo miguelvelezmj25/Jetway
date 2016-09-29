ba3ad869e22725aa77e04b4b8039fd0a8eb5e9cc
Miguel Velez <miguelvelez@mijecu25.com>
2016-09-29 19:36:01 -0400

    Refactor

--- measure_sac.py
+++ measure_sac.py
@@ -15,121 +15,139 @@ import hashlib
 
 
 def ex(p):
-	return subprocess.call(p, shell=True)
+    return subprocess.call(p, shell=True)
 
 
-#use symbolic links to map powermeter readings to local files if necessary
+# use symbolic links to map powermeter readings to local files if necessary
 home = os.path.expanduser("~")+"/"
-workingDir = home+"energy/"
-powermeterPath = workingDir+"energy.log"
-cpumeterPath = home+"/cpu.log"
+working_dir = home + "energy/"
+power_meter_path = working_dir + "energy.log"
+cpu_meter_path = home + "/cpu.log"
 
-tmpPowermeterPath = workingDir+".energy.log.tmp"
-tmpCpumeterPath = workingDir+".cpu.log.tmp"
+tmp_power_meter_path = working_dir + ".energy.log.tmp"
+tmp_cpu_meter_path = working_dir + ".cpu.log.tmp"
 
 timeout = "20m"
 
-msrDir = workingDir+"sac_results/"
+msr_dir = working_dir + "sac_results/"
 
-SACOUT = workingDir+"nbody/tmp.out"
-SACCOMPILE = workingDir+"sac-compile.sh"
-SACRUN = workingDir+"sac-run.sh"
+SACOUT = working_dir + "nbody/tmp.out"
+SACCOMPILE = working_dir + "sac-compile.sh"
+SACRUN = working_dir + "sac-run.sh"
 
 ex("sh CPU_monitor.sh start")
-ex("mkdir -p "+workingDir)
-ex("mkdir -p "+msrDir)
-os.chdir(workingDir)
+ex("mkdir -p " + working_dir)
+ex("mkdir -p " + msr_dir)
+os.chdir(working_dir)
 assert os.path.isfile(SACCOMPILE)
 assert os.path.isfile(SACRUN)
-print "starting measuring process. make sure power readings are activated and directed to "+powermeterPath
-
-def readAvgLogValue(logFile):
-	f = open(logFile)
-	fline = f.readline()
-	ftotal = 0
-	fcount = 0
-	while fline:
-		fcount = fcount + 1
-		ftotal = ftotal + float(fline)
-		fline = f.readline()
-	if fcount > 0:
-		cpu = str( ftotal / fcount)
-	else: 
-		cpu = "-1"
-	return cpu
-
-def readLogFile(logfile):
-	myvars = {}
-	assert os.path.isfile(logfile)
-	with open(logfile) as myfile:
-		for line in myfile:
-			if ":" in line:
-				name, var = line.partition(":")[::2]
-				myvars[name.strip()] = var.strip()
-	return myvars
-	
-
-def hashfile(f):
-        return hashlib.sha1(open(f, 'rb').read()).hexdigest()
-
-def measure(logfile, cmd, extraGatherResults=None):
-        command = "time -f \"real:%e\nuser:%U\nsys:%S\nexit:%x\nioin:%I\nioout:%O\nmaxmem:%M\navgmem:%K\" -o "+logfile+" -a timeout " + timeout + " " + cmd 
-	#discard old powermeter measurements
-	ex(">{0}; >{1}; >{2}".format(powermeterPath, cpumeterPath,logfile))
-
-	exitcode = ex(command)
-
-	ex("sed -e 's/^.*: \([0-9]*\)$/\\1/' < {0} | grep \"^[0-9]*$\" > {1}".format(powermeterPath, tmpPowermeterPath))
-	ex("cat "+tmpPowermeterPath)
-	ex("cp {0} {1}".format(cpumeterPath, tmpCpumeterPath))
-	avgCpu=readAvgLogValue(tmpCpumeterPath)
-	avgPower=readAvgLogValue(tmpPowermeterPath)
-	results = { "cpu": avgCpu, "power":avgPower, "exit": exitcode }
-	if extraGatherResults!=None:
-		results.update(extraGatherResults())
-	results.update(readLogFile(logfile))
-	return results
+print "starting measuring process. make sure power readings are activated and directed to " + power_meter_path
 
 
+def read_avg_log_value(log_file):
+    f = open(log_file)
+    f_line = f.readline()
+    f_total = 0
+    f_count = 0
 
-#perform a measurement for a specific configuration (in a series) and 
-#returns a list of measurement results (map from NFP name to value)
-#
-#the method is called by the mcontrol infrastructure
-#the actual measurement depends heavily on the actual program
-#being measured.
-#in this case, both the compilation and the running of the compiled
-#program are measured. it used measurements from `time` as well as
-#measurement from external CPU meters and power meters
-def measureSaC(seriesName, configId):
-	param = get_config_params(configId)
-	print "\n*** measuring {0} ({1})".format(configId, param)
+    while f_line:
+        f_count += 1
+        f_total += float(f_line)
+        f_line = f.readline()
 
-	time.sleep(5)	
-	compileResults = measure(msrDir+".compilelog."+seriesName,SACCOMPILE+" "+param)
-	if compileResults["exit"] != "0":
-		print "compilation failed."
-		runResults = {}
-	else:
-		compileResults["size"]=os.path.getsize(SACOUT)
-		hashv=hashfile(SACOUT)
-		compileResults["hash"]=hashv	
-		runlogFile=msrDir+".runlog."+seriesName+"."+hashv
-		if os.path.isfile(runlogFile):
-			print "same binary measured earlier, skipping"
-			runResults = {}
-		else:
-			time.sleep(5)
-			runResults = measure(runlogFile, SACRUN)
-	results = {}
-	results.update(dict(("compile-"+k, v) for (k,v) in compileResults.items()))
-	results.update(dict(("run-"+k, v) for (k,v) in runResults.items()))
-	print str(results)
-	return results
+    if f_count > 0:
+        cpu = str(f_total / f_count)
+    else:
+        cpu = "-1"
+
+    return cpu
 
 
+def read_log_file(log_file):
+    my_vars = {}
+    assert os.path.isfile(log_file)
 
-if len(sys.argv)<=1:
-	print "expecting measurement series as parameter"
-	sys.exit(1)
-m_control(sys.argv[1:], measureSaC)
+    with open(log_file) as my_file:
+        for line in my_file:
+            if ":" in line:
+                name, var = line.partition(":")[::2]
+                my_vars[name.strip()] = var.strip()
+
+    return my_vars
+
+
+def hash_file(f):
+    return hashlib.sha1(open(f, 'rb').read()).hexdigest()
+
+
+def measure(log_file, cmd, extra_gather_results=None):
+    command = "time -f \"real:%e\nuser:%U\nsys:%S\nexit:%x\nioin:%I\nioout:%O\nmaxmem:%M\navgmem:%K\" -o " \
+              + log_file + " -a timeout " + timeout + " " + cmd
+
+    # discard old powermeter measurements
+    ex(">{0}; >{1}; >{2}".format(power_meter_path, cpu_meter_path, log_file))
+
+    exitcode = ex(command)
+
+    ex("sed -e 's/^.*: \([0-9]*\)$/\\1/' < {0} | grep \"^[0-9]*$\" > {1}".format(power_meter_path, tmp_power_meter_path))
+    ex("cat " + tmp_power_meter_path)
+    ex("cp {0} {1}".format(cpu_meter_path, tmp_cpu_meter_path))
+    avg_cpu=read_avg_log_value(tmp_cpu_meter_path)
+    avg_power=read_avg_log_value(tmp_power_meter_path)
+    results = {"cpu": avg_cpu, "power":avg_power, "exit": exitcode}
+
+    if extra_gather_results is not None:
+        results.update(cmd)
+
+    results.update(read_log_file(log_file))
+
+    return results
+
+
+def measure_sa_c(series_name, config_id):
+    """
+    perform a measurement for a specific configuration (in a series) and
+    returns a list of measurement results (map from NFP name to value)
+
+    the method is called by the mcontrol infrastructure
+    the actual measurement depends heavily on the actual program
+    being measured.
+    in this case, both the compilation and the running of the compiled
+    program are measured. it used measurements from `time` as well as
+    measurement from external CPU meters and power meters
+    """
+    param = get_config_params(config_id)
+    print "\n*** measuring {0} ({1})".format(config_id, param)
+
+    time.sleep(5)
+    compile_results = measure(msr_dir + ".compilelog." + series_name, SACCOMPILE + " " + param)
+
+    if compile_results["exit"] != "0":
+        print "compilation failed."
+        run_results = {}
+    else:
+        compile_results["size"]=os.path.getsize(SACOUT)
+        hash_v = hash_file(SACOUT)
+        compile_results["hash"] = hash_v
+        run_log_file = msr_dir + ".runlog." + series_name + "." + hash_v
+
+        if os.path.isfile(run_log_file):
+            print "same binary measured earlier, skipping"
+            run_results = {}
+        else:
+            time.sleep(5)
+            run_results = measure(run_log_file, SACRUN)
+
+    results = {}
+    results.update(dict(("compile-"+k, v) for (k,v) in compile_results.items()))
+    results.update(dict(("run-"+k, v) for (k,v) in run_results.items()))
+    print str(results)
+
+    return results
+
+
+if len(sys.argv) <= 1:
+    print "expecting measurement series as parameter"
+    sys.exit(1)
+
+m_control(sys.argv[1:], measure_sa_c)
