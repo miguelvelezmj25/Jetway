8b58d8cba2267ae236b72698e089d80a2c12740e
Miguel Velez <miguelvelez@mijecu25.com>
2016-10-13 21:18:02 -0400

    Update tables

--- jetway/mdb.py
+++ jetway/mdb.py
@@ -9,16 +9,16 @@ series_id_cache = {}
 nfp_id_cache = {}
 
 
-def startup():
+def startup(db):
     config_parser = ConfigParser.RawConfigParser()
     config_file_path = r'.dbconfig'
     config_parser.read(config_file_path)
 
     global connection, cursor
-    connection = connector.connect(host=config_parser.get('db', 'hostname'),  # your host, usually localhost
-                                   user=config_parser.get('db', 'user'),  # your username
-                                   passwd=config_parser.get('db', 'password'),  # your password
-                                   db=config_parser.get('db', 'database'))  # name of the data base
+    connection = connector.connect(host=config_parser.get(db, 'hostname'),  # your host, usually localhost
+                                   user=config_parser.get(db, 'user'),  # your username
+                                   passwd=config_parser.get(db, 'password'),  # your password
+                                   db=config_parser.get(db, 'database'))  # name of the data base
     cursor = connection.cursor()
 
 
@@ -52,8 +52,9 @@ def exec_sql(sql):
     return r
 
 
-def select_id_where_options(table, options):
-    statement = 'select id from ' + table + ' where options = "' + options + '"'
+def select_ids(sql):
+    statement = 'select id ' + sql
+    print statement
 
     ids = []
     for element in exec_sql(statement):
@@ -62,30 +63,83 @@ def select_id_where_options(table, options):
     return ids
 
 
-def select_where(table, clause):
-    statement = 'select * from ' + table + ' where ' + clause
-
-    results = []
-    for result in exec_sql(statement):
-        new_result = []
-
-        i = 0
-        while i < len(result):
-            new_result.append(str(result[i]))
-            i += 1
-
-        results.append(tuple(new_result))
-
-    return results
-
-
 def insert(table, columns, values):
-    statement = 'insert into ' + table + ' (' + columns + ') values ' + '({0})'.format(values)
+    statement = 'insert into ' + table + ' (' + columns + ') values ({0})'.format(values)
     print statement
 
     cursor.execute(statement)
     connection.commit()
 
+
+def get_nfp_id(nfp):
+    """
+    looks up an nfp, creates that nfp if it does not exist
+    """
+    if nfp not in nfp_id_cache:
+        statement = 'select id from nfps where name="{0}"'.format(nfp)
+        print statement
+        cursor.execute(statement)
+        r = cursor.fetchone()
+
+        if r is None:
+            print "creating new nfp: "+nfp
+            statement = 'insert into nfps (Name) values ("{0}")'.format(nfp)
+            print statement
+
+            cursor.execute(statement)
+            connection.commit()
+            statement = 'select id from nfps where name="{0}"'.format(nfp)
+            print statement
+
+            cursor.execute(statement)
+            r = cursor.fetchone()
+
+        nfp_id_cache[nfp] = r[0]
+
+    return nfp_id_cache[nfp]
+
+
+def add_configuration(option):
+    statement = 'insert into configurations (options) values ("{0}")'.format(option)
+    print statement
+
+    cursor.execute(statement)
+    connection.commit()
+
+
+def get_configurations_like_option(option):
+    statement = 'select * from configurations where options like "{0}"'.format(option)
+    print statement
+
+    configurations = []
+    for element in exec_sql(statement):
+        configurations.append(element)
+
+    return configurations
+
+
+def get_next_todo():
+    statement = 'select * from todos order by priority limit 1'
+    print statement
+
+    cursor.execute(statement)
+    result = cursor.fetchall()
+    id = result[0][0]
+    iterations = result[0][1]
+
+    if id:
+        if iterations > 1:
+            statement = 'update todos set iterations = iterations - 1 where configuration_id = {0}'.format(id)
+        else:
+            statement = 'delete from todos where configuration_id = {0}'.format(id)
+
+        print statement
+        connection.commit()
+
+    return id
+
+
+############### NOT TESTED ###############
 
 def get_series_id(series_name):
     """
@@ -105,34 +159,6 @@ def get_series_id(series_name):
         series_id_cache[series_name] = r[0]
 
     return series_id_cache[series_name]
-
-
-def get_nfp_id(nfp):
-    """
-    looks up an nfp, creates that nfp if it does not exist
-    """
-    if nfp not in nfp_id_cache:
-        statement = 'select id from nfps where name="' + nfp + '"'
-        print statement
-        cursor.execute(statement)
-        r = cursor.fetchone()
-
-        if r is None:
-            print "creating new nfp: "+nfp
-            statement = 'insert into nfps (Name) values ("' + nfp + '")'
-            print statement
-
-            cursor.execute(statement)
-            connection.commit()
-            statement = 'select id from nfps where name="' + nfp + '"'
-            print statement
-
-            cursor.execute(statement)
-            r = cursor.fetchone()
-
-        nfp_id_cache[nfp] = r[0]
-
-    return nfp_id_cache[nfp]
 
 
 def store_measurements(series_name, config_id, result_map):
