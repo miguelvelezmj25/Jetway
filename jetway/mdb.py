55328a4377e05b8b5a8aa40c077c160a6d1deb2d
Miguel Velez <miguelvelez@mijecu25.com>
2016-10-19 14:40:59 -0400

    Fix bug in statement

--- jetway/mdb.py
+++ jetway/mdb.py
@@ -120,7 +120,7 @@ def get_configurations_like_option(option):
 
 def get_next_todo(column=None, value=None):
     if column is not None and value is not None:
-        statement = 'select * from todos where {0}} = {1} order by priority limit 1'.format(column, value)
+        statement = 'select * from todos where {0} = {1} order by priority limit 1'.format(column, value)
     else:
         statement = 'select * from todos order by priority limit 1'
     print statement
