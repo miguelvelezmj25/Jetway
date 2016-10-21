02a73f8a231132ab14cbc5639c0eb4a7b0ac386d
Miguel Velez <miguelvelez@mijecu25.com>
2016-10-21 16:02:54 -0400

    Check if worker is empty

--- jetway/mdb.py
+++ jetway/mdb.py
@@ -175,7 +175,7 @@ def get_next_todo(column=None, value=None):
     if column is not None and value is not None:
         statement = 'select * from todos where {0} = {1} order by priority limit 1'.format(column, value)
     else:
-        statement = 'select * from todos where worker is NULL order by priority limit 1'
+        statement = 'select * from todos where worker is NULL or worker = "" order by priority limit 1'
     print statement
 
     cursor.execute(statement)
